c8ab373c7f9617baab2b97066820a2fabb01443c
Ardeshir Saadat <ardeshirsaadat@yahoo.com>
2022-06-20 14:53:57 -0400

    change localhost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0705267ac3084c9a931b64ebfbdf20d-1216355855.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
